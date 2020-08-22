a291b2a0ca96752308690f6fb2b00abe784bd046
Gaston Tarango <gaston.tarango@gmail.com>
2020-08-21 23:13:36 -0500

    .

--- JS/JS_basic.js
+++ JS/JS_basic.js
@@ -75,9 +75,17 @@ if (keyPressed == 'r') {
 
 let endPos =5;
 var pos = 0;
+var enemyPos = 4;
+var isGameOver = false;
 
-while (pos < endPos) {
-    pos++;
+// while (pos < endPos) {
+//     pos++;
+// }
+
+while (!isGameOver) {
+    xPos++;
+    if (xPos >= endPos || xPos == enemyPos) {
+        isGameOver = true;
+    }
 }
-
 
